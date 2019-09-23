9f74f7b9c3815ed0ca73e3ede93fc70aede5904e
Gary Anderson <ganderson@seattleacademy.org>
2019-09-23 06:00:40 -0700

    Add face uploader code

---
+++ faces.php
@@ -0,0 +1,19 @@
+<?php
+// Change $dir to "faces/" and chown :www-data faces to make local directory
+$dir = "/faces/";
+$return_array = array();
+
+if(is_dir($dir)){
+    if($dh = opendir($dir)){
+        while(($file = readdir($dh)) != false){
+            if($file == "." or $file == ".."){
+            } else {
+                $return_array[] = $file; // Add the file to the array
+            }
+        }
+    }
+
+    echo json_encode($return_array);
+}
+
+?>
